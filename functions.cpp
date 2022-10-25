cac07195e9686d14f9d9797478b7fc1b6d9dba32
CarterCaleb83 <cartercaleb83@gmail.com>
2022-10-25 15:18:43 -0700

    Added caleb() function

--- functions.cpp
+++ functions.cpp
@@ -4,3 +4,8 @@ void ekaterina()
 {
     cout <<"Hello, my name is Ekaterina\n";
 }
+
+void caleb()
+{
+    cout <<"Hello, my name is Caleb\n";
+}
--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main()
 {
     ekaterina();
+    caleb();
     return 0;
 }
