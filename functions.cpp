ed2dd9b31c4e3c55929899b9a8883c39e5932397
DapperNurd <jbenbonus@gmail.com>
2022-10-25 15:20:24 -0700

    added ben function with cout

--- functions.cpp
+++ functions.cpp
@@ -4,3 +4,7 @@ void ekaterina()
 {
     cout <<"Hello, my name is Ekaterina\n";
 }
+
+void ben() {
+    cout << "Hello, my name Ben" << endl;
+}
--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main()
 {
     ekaterina();
+    ben();
     return 0;
 }
