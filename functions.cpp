51e80f89637761daa405694459503df7fb681a39
Ekaterina-Miller <miller_ekaterina@yahoo.com>
2022-10-25 14:18:54 -0700

    Initial commit

---
+++ functions.cpp
@@ -0,0 +1,6 @@
+#include "functions.h"
+
+void ekaterina()
+{
+    cout <<"Hello, my name is Ekaterina\n";
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "functions.h"
+
+int main()
+{
+    ekaterina();
+    return 0;
+}
