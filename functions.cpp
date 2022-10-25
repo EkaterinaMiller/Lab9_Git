8881a59f1f2d754641e5a7b20c4b7b4c858eaed2
MediumWaffle <aaronjarnes@gmail.com>
2022-10-25 15:19:10 -0700

    aaron's func

--- functions.cpp
+++ functions.cpp
@@ -4,3 +4,11 @@ void ekaterina()
 {
     cout <<"Hello, my name is Ekaterina\n";
 }
+
+void aaron(){
+    cout << "\n ____________________________________ \n";
+    cout << "|  ________________________________  |\n";
+    cout << "| | dont look at the readme -aaron | |\n";
+    cout << "| |________________________________| |\n";
+    cout << "|____________________________________|\n\n";
+}
--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main()
 {
     ekaterina();
+    aaron();
     return 0;
 }
